8ffb0c58dd3a395d0ad77fbad29f5f3511069837
Dhaval Sojitra <dhavalsojitra875@gmail.com>
2024-06-27 16:35:40 +0530

    task2

---
+++ Task2.js
@@ -0,0 +1,16 @@
+function task(sqrfeet) {
+    let cement = 50
+    let cent = 100
+    let kapchi = 150
+    let water = 50
+    let chemical = 0.2
+    console.log("Total Sqrfeet : " + sqrfeet)
+    console.log("------------------------------------")
+    console.log("Cement : " + sqrfeet * cement)
+    console.log("Cent : " + sqrfeet * cent)
+    console.log("kapchi : " + sqrfeet * kapchi)
+    console.log("Water : " + sqrfeet * water)
+    console.log("Chemical : " + sqrfeet * chemical)
+}
+let sqrfeet = 100
+task(sqrfeet)
