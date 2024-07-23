3d49384a5b462b89cb48ee9e52635dec5393fd87
unknown <dhavalsojitra875@gmail.com>
2024-07-23 09:08:25 +0530

    added

--- Route/server.js
+++ Route/server.js
@@ -6,11 +6,11 @@ app.get('/products', (req, res) => {
     res.json({
         "status": "success",
         "data": [
-            { "id": "1", "name": "HP BAJJ32", "price": 45000, "category": "laptop" },
-            { "id": "2", "name": "HP BAJJ32", "price": 45000, "category": "laptop" },
-            { "id": "3", "name": "HP BAJJ32", "price": 45000, "category": "laptop" },
-            { "id": "4", "name": "HP BAJJ32", "price": 45000, "category": "laptop" },
-            { "id": "5", "name": "HP BAJJ32", "price": 45000, "category": "laptop" }
+            { "id": "1", "name": "HP", "price": 5000, "category": "laptop" },
+            { "id": "2", "name": "HP", "price": 5000, "category": "laptop" },
+            { "id": "3", "name": "HP", "price": 5000, "category": "laptop" },
+            { "id": "4", "name": "HP", "price": 5000, "category": "laptop" },
+            { "id": "5", "name": "HP", "price": 5000, "category": "laptop" }
         ],
         "message": "retrieve successfully"
     });
@@ -20,7 +20,7 @@ app.get('/product/:id', (req, res) => {
     const id = req.params.id;
     res.json({
         "status": "success",
-        "data": { "id": id, "name": "HP BAJJ32", "price": 45000, "category": "laptop" },
+        "data": { "id": id, "name": "HP", "price": 45000, "category": "laptop" },
         "message": "retrieve successfully"
     });
 });
