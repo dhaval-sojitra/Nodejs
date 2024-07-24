1723401363c3f438900f8e55ab07d38e3a99202b
unknown <dhavalsojitra875@gmail.com>
2024-07-24 08:48:20 +0530

    added

--- Route/server.js
+++ Route/server.js
@@ -1,52 +1,8 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+const router = require('./products.route')
 
-app.get('/products', (req, res) => {
-    res.json({
-        "status": "success",
-        "data": [
-            { "id": "1", "name": "HP", "price": 5000, "category": "laptop" },
-            { "id": "2", "name": "HP", "price": 5000, "category": "laptop" },
-            { "id": "3", "name": "HP", "price": 5000, "category": "laptop" },
-            { "id": "4", "name": "HP", "price": 5000, "category": "laptop" },
-            { "id": "5", "name": "HP", "price": 5000, "category": "laptop" }
-        ],
-        "message": "retrieve successfully"
-    });
-});
 
-app.get('/product/:id', (req, res) => {
-    const id = req.params.id;
-    res.json({
-        "status": "success",
-        "data": { "id": id, "name": "HP", "price": 45000, "category": "laptop" },
-        "message": "retrieve successfully"
-    });
-});
-
-app.post('/product', (req, res) => {
-    res.json({
-        "status": "success",
-        "data": { "name": req.body.name, "price": req.body.price, "category": req.body.category },
-        "message": "insert successfully"
-    });
-});
-
-app.put('/product', (req, res) => {
-    res.json({
-        "status": "success",
-        "data": { "id": req.body.id, "name": req.body.name, "price": req.body.price, "category": req.body.category },
-        "message": "update successfully"
-    });
-});
-
-app.delete('/product', (req, res) => {
-    res.json({
-        "status": "success",
-        "data": { "id": req.body.id },
-        "message": "deleted successfully"
-    });
-});
-
+app.use('/api', router)
 app.listen(5000, () => { console.log('Running in 5000'); });
